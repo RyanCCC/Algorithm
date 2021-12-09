be7bc6f0da4f754a6fca0aa06f5a67d3db03f508
chenjl <chenjl@augurit.com>
2021-12-09 17:09:40 +0800

    opencv code

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,16 @@
 ﻿#include <iostream>
+#include<opencv2/core/core.hpp>
+#include<opencv2/highgui/highgui.hpp>
 
+using namespace std;
+using namespace cv;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string img_path = "./src/test.png";
+    Mat img = imread(img_path);
+    namedWindow("Test");
+    imshow("Test", img);
+    waitKey(0);
+    return 0;
 }
