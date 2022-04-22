e7fc35971c9a1754f7cb8abc8981c86a63aee51c
chenjl <chenjl@augurit.com>
2022-04-22 09:41:45 +0800

    add patch match

--- Main.cpp
+++ Main.cpp
@@ -9,6 +9,10 @@
 #include "header/pfs.h"
 #include "header/D8.h"
 
+#include "masked_image.h"
+#include "nnf.h"
+#include "inpaint.h"
+
 using namespace std;
 using namespace cv;
 
@@ -19,6 +23,27 @@ extern void videoshow(string video);
 
 int main()
 {
+    //Image inpaint
+    auto source = cv::imread("./src/forest_pruned.bmp", cv::IMREAD_COLOR);
+
+    auto mask = cv::Mat(source.size(), CV_8UC1);
+    mask = cv::Scalar::all(0);
+    for (int i = 0; i < source.size().height; ++i) {
+        for (int j = 0; j < source.size().width; ++j) {
+            auto source_ptr = source.ptr<unsigned char>(i, j);
+            if (source_ptr[0] == 255 && source_ptr[1] == 255 && source_ptr[2] == 255) {
+                mask.at<unsigned char>(i, j) = 1;
+            }
+        }
+    }
+    auto metric = PatchSSDDistanceMetric(3);
+    auto result = Inpainting(source, mask, &metric).run(true, true);
+    //展示图像
+    cv::imwrite("./images/forest_recovered.bmp", result);
+    cv::imshow("Result", result);
+    cv::waitKey();
+
+
     D8_main();
     pfs();
     string img1 = "./src/lena.jpg";
