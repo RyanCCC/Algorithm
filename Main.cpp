0e954349ff67b2391c79e71a5f8b0a5dd58508cf
chenjl <chenjl@augurit.com>
2022-04-24 14:47:05 +0800

    添加github索引

--- Main.cpp
+++ Main.cpp
@@ -23,21 +23,28 @@ extern void videoshow(string video);
 
 int main()
 {
-    //Image inpaint
-    auto source = cv::imread("./src/forest_pruned.bmp", cv::IMREAD_COLOR);
+    //Image inpaint https://github.com/vacancy/PyPatchMatch
+    auto source = cv::imread("./src/202112311304288.jpg", cv::IMREAD_COLOR);
+    auto mask_ori = cv::imread("./src/202112311304288_mask.jpg");
 
-    auto mask = cv::Mat(source.size(), CV_8UC1);
+    auto mask = cv::Mat(mask_ori.size(), CV_8UC1);
     mask = cv::Scalar::all(0);
-    for (int i = 0; i < source.size().height; ++i) {
-        for (int j = 0; j < source.size().width; ++j) {
-            auto source_ptr = source.ptr<unsigned char>(i, j);
-            if (source_ptr[0] == 255 && source_ptr[1] == 255 && source_ptr[2] == 255) {
-                mask.at<unsigned char>(i, j) = 1;
+    for (int i = 0; i < mask_ori.size().height; ++i) {
+        for (int j = 0; j < mask_ori.size().width; ++j) {
+            auto source_ptr = mask_ori.ptr<unsigned char>(i, j);
+            if (source_ptr[0] !=0 && source_ptr[1] != 0 && source_ptr[2] != 0) {
+                mask.at<unsigned char>(i, j) = 255;
             }
         }
     }
+
+
+    cv::imshow("mask", mask);
+    cv::waitKey();
+
+
     auto metric = PatchSSDDistanceMetric(3);
-    auto result = Inpainting(source, mask, &metric).run(true, true);
+    auto result = Inpainting(source, mask, &metric).run(true, false);
     //展示图像
     cv::imwrite("./images/forest_recovered.bmp", result);
     cv::imshow("Result", result);
